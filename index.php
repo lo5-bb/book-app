09a0a47dbeb9a9643fce70678d29a7c170b88b00
codecalm <codecalm@gmail.com>
2015-04-27 23:08:05 +0200

    porpawki typo

--- index.php
+++ index.php
@@ -11,19 +11,17 @@
 	<link rel="stylesheet"
 		  href="//fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic&subset=latin,latin-ext&subset=latin,latin-ext"/>
 
-
 </head>
 <body>
 
 <div class="book-container">
 	<main class="book typo">
 
-		<h1>Am I right?</h1>
+		<h1>Wstęp</h1>
 
-		<p>I don't want to talk to you no more, you empty-headed animal food trough water! I fart in your general
-			direction! Your mother was a hamster and your father smelt of elderberries! Now leave before I am forced to
-			taunt you a second time! Well, she turned me into a newt. Shut up! Well, how'd you become king, then? But
-			you are dressed as one&hellip; Did you dress her up like this?</p>
+		<p>Internet jest obecnie używany za najważniejsze źródło informacji. Dlatego należy poznać dobrze narzędzia i metody umożliwiające właściwe jego wykorzystanie. Właśnie w tej publikacji, przeznaczonej dla uczniów V Liceum Ogólnokształcącego, staramy się przybliżyć wam narzędzia i metody, dzięki którym możecie być nie tylko biernymi odbiorcami informacji internetowych, ale także ich twórcami. Ta książka pozwoli wam zapoznać się z funkcjonowaniem jednej z najpopularniejszych usług internetowych. Czytając poszczególne rozdziały będziecie wprowadzani w kolejne etapy związane z technikami tworzenia stron WWW. Oczywiście samo czytanie nie wystarczy, zachęcamy do ćwiczeń w ramach zajęć prowadzonych w szkole, ale także do samodzielnej pracy.</p>
+
+		<p>Korzystając z tej publikacji oraz dostępu do szkolnego serwera możecie po- znawać i ćwiczyć zastosowanie kolejnych elementów używanych na stronach WWW. Zachęcamy do samodzielnej lektury i aktywnego udziału w zajęciach.</p>
 
 		<h2>Makes Ben Hur look like an Epic!</h2>
 
@@ -32,7 +30,14 @@
 		<ul>
 			<li>No, no, no! Yes, yes. A bit. But she's got a wart.</li>
 			<li>Well, we did do the nose.</li>
-			<li>No, no, no! Yes, yes. A bit. But she's got a wart.</li>
+			<li>No, no, no! Yes, yes. A bit. But she's got a wart.
+				<ol>
+					<li>Well, she turned me into a newt.</li>
+					<li>And this isn't my nose. This is a false one.</li>
+					<li>We found them.</li>
+					<li>Well, Mercia's a temperate zone!</li>
+				</ol>
+			</li>
 		</ul>
 		<h3>Blue. No, yel&hellip;</h3>
 
@@ -97,7 +102,101 @@
 		<p>I have to push the pram a lot. And the hat. She's a witch! Bring her forward! Oh! Come and see the violence
 			inherent in the system! Help, help, I'm being repressed!</p>
 
+		<ul>
+			<li>
+				<code>Ctrl + Alt + M</code> - automerge as much as possible
+			</li>
+			<li>
+				<code>b</code> - jump to next merge conflict
+			</li>
+			<li>
+				<code>s</code> - change the order of the conflicted lines
+			</li>
+			<li>
+				<code>u</code> - undo a merge
+			</li>
+			<li>
+				<code>left mouse button</code> - mark a block to be the winner
+			</li>
+			<li>
+				<code>middle mouse button</code> - mark a line to be the winner
+			</li>
+			<li>
+				<code>Ctrl + S</code> - save
+			</li>
+			<li>
+				<code>Ctrl + Q</code> - quit
+			</li>
+		</ul>
+
 		<p>Burn her! Bloody Peasant! Oh, ow! Burn her anyway! Where'd you get the coconuts?</p>
+
+		<ul>
+			<li>
+				<strong>ajax</strong>: All AJAX functionality: <code>$.ajax()</code>, <code>$.get()</code>, <code>$.post()</code>,
+				<code>$.ajaxSetup()</code>, <code>.load()</code>, transports, and ajax event shorthands such as <code>.ajaxStart()</code>.
+			</li>
+			<li>
+				<strong>ajax/xhr</strong>: The XMLHTTPRequest AJAX transport only.
+			</li>
+			<li>
+				<strong>ajax/script</strong>: The <code>&lt;script&gt;</code> AJAX transport only; used to retrieve
+				scripts.
+			</li>
+			<li>
+				<strong>ajax/jsonp</strong>: The JSONP AJAX transport only; depends on the ajax/script transport.
+			</li>
+			<li>
+				<strong>css</strong>: The <code>.css()</code> method plus non-animated <code>.show()</code>, <code>.hide()</code>
+				and <code>.toggle()</code>. Also removes <strong>all</strong> modules depending on css (including
+				<strong>effects</strong>, <strong>dimensions</strong>, and <strong>offset</strong>).
+			</li>
+			<li>
+				<strong>deprecated</strong>: Methods documented as deprecated but not yet removed.
+			</li>
+			<li>
+				<strong>dimensions</strong>: The <code>.width()</code> and <code>.height()</code> methods, including
+				<code>inner-</code> and <code>outer-</code> variations.
+			</li>
+			<li>
+				<strong>effects</strong>: The <code>.animate()</code> method and its shorthands such as
+				<code>.slideUp()</code> or <code>.hide("slow")</code>.
+			</li>
+			<li>
+				<strong>event</strong>: The <code>.on()</code> and <code>.off()</code> methods and all event
+				functionality. Also removes <code>event/alias</code>.
+			</li>
+			<li>
+				<strong>event/alias</strong>: All event attaching/triggering shorthands like <code>.click()</code> or
+				<code>.mouseover()</code>.
+			</li>
+			<li>
+				<strong>offset</strong>: The <code>.offset()</code>, <code>.position()</code>,
+				<code>.offsetParent()</code>, <code>.scrollLeft()</code>, and <code>.scrollTop()</code> methods.
+			</li>
+			<li>
+				<strong>wrap</strong>: The <code>.wrap()</code>, <code>.wrapAll()</code>, <code>.wrapInner()</code>, and
+				<code>.unwrap()</code> methods.
+			</li>
+			<li>
+				<strong>core/ready</strong>: Exclude the ready module if you place your scripts at the end of the body.
+				Any ready callbacks bound with <code>jQuery()</code> will simply be called immediately. However, <code>jQuery(document).ready()</code>
+				will not be a function and <code>.on("ready", ...)</code> or similar will not be triggered.
+			</li>
+			<li>
+				<strong>deferred</strong>: Exclude jQuery.Deferred. This also removes jQuery.Callbacks. <em>Note</em>
+				that modules that depend on jQuery.Deferred(AJAX, effects, core/ready) will not be removed and will
+				still expect jQuery.Deferred to be there. Include your own jQuery.Deferred implementation or exclude
+				those modules as well (<code>grunt custom:-deferred,-ajax,-effects,-core/ready</code>).
+			</li>
+			<li>
+				<strong>exports/global</strong>: Exclude the attachment of global jQuery variables ($ and jQuery) to the
+				window.
+			</li>
+			<li>
+				<strong>exports/amd</strong>: Exclude the AMD definition.
+			</li>
+		</ul>
 
 		<p>Well, I didn't vote for you. He hasn't got shit all over him. Well, she turned me into a newt. Why? The
 			Knights Who Say Ni demand a sacrifice! We want a shrubbery!!</p>
@@ -110,6 +209,8 @@
 			basis for a system of government. Supreme executive power derives from a mandate from the masses, not from
 			some farcical aquatic ceremony.</p>
 
+		<img src="assets/img/img-1.jpeg" alt=""/>
+
 		<p>Shut up! Will you shut up?! Listen. Strange women lying in ponds distributing swords is no basis for a system
 			of government. Supreme executive power derives from a mandate from the masses, not from some farcical
 			aquatic ceremony. Why? Now, look here, my good man. Well, how'd you become king, then?</p>
@@ -118,14 +219,23 @@
 			yet these are not strangers to our land. Shh! Knights, I bid you welcome to your new home. Let us ride to
 			Camelot! Why? Shut up! Will you shut up?! I'm not a witch. But you are dressed as one&hellip;</p>
 
+		<figure>
+			<img src="assets/img/img-2.jpeg" alt=""/>
+			<figcaption>Did you dress her up like this? Well, Mercia's a temperate zone!</figcaption>
+		</figure>
+
 		<p>Where'd you get the coconuts? Did you dress her up like this? Well, Mercia's a temperate zone! We shall say
 			'Ni' again to you, if you do not appease us. Look, my liege! Shut up! Will you shut up?!</p>
 
-		<p>You can't expect to wield supreme power just 'cause some watery tart threw a sword at you! &hellip;Are you
-			suggesting that coconuts migrate? I dunno. Must be a king. Well, she turned me into a newt.</p>
+		<blockquote>
+			<p>You can't expect to wield supreme power just 'cause some watery tart threw a sword at you! &hellip;Are
+				you
+				suggesting that coconuts migrate? I dunno. Must be a king. Well, she turned me into a newt.</p>
 
-		<p>Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! I dunno. Must be a king. Well, what
-			do you want? I am your king. How do you know she is a witch?</p>
+			<p>Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! I dunno. Must be a king. Well,
+				what
+				do you want? I am your king. How do you know she is a witch?</p>
+		</blockquote>
 
 		<p>The Knights Who Say Ni demand a sacrifice! Look, my liege! Be quiet! Shh! Knights, I bid you welcome to your
 			new home. Let us ride to Camelot! Now, look here, my good man. Look, my liege!</p>

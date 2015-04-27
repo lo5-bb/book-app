da8d805d082d4c1ea3965fa5aa37f65aff894d22
codecalm <codecalm@gmail.com>
2015-04-27 22:16:47 +0200

    pierwsze typo

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// pluginy
+var gulp            = require('gulp'),
+    gutil           = require('gulp-util'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    connect         = require('gulp-connect'),
+    sass         	= require('gulp-sass');
+
+gulp.task('watch', function () {
+    gulp.watch([ './assets/scss/**/*.scss'], ['sass']);
+});
+
+
+// sass
+gulp.task('sass', function () {
+    return gulp.src('./assets/scss/**/*.scss')
+        .pipe(sass({
+            onError: function (error) {
+                gutil.log(gutil.colors.red(error));
+                gutil.beep();
+            },
+            onSuccess: function () {
+                gutil.log(gutil.colors.green('Sass styles compiled successfully.'));
+            }
+        }))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('serve', ['sass', 'watch']);
--- index.php
+++ index.php
@@ -1,1 +1,142 @@
-index
+<!doctype html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<title>LO5 Książka</title>
+	<meta name="viewport" content="initial-scale=1">
+	<link rel="stylesheet" href="assets/css/style.css"/>
+
+	<link rel="stylesheet"
+		  href="//fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic&subset=latin,latin-ext"/>
+	<link rel="stylesheet"
+		  href="//fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic&subset=latin,latin-ext&subset=latin,latin-ext"/>
+
+
+</head>
+<body>
+
+<div class="book-container">
+	<main class="book typo">
+
+		<h1>Am I right?</h1>
+
+		<p>I don't want to talk to you no more, you empty-headed animal food trough water! I fart in your general
+			direction! Your mother was a hamster and your father smelt of elderberries! Now leave before I am forced to
+			taunt you a second time! Well, she turned me into a newt. Shut up! Well, how'd you become king, then? But
+			you are dressed as one&hellip; Did you dress her up like this?</p>
+
+		<h2>Makes Ben Hur look like an Epic!</h2>
+
+		<p>Now, look here, my good man. And this isn't my nose. This is a false one. I dunno. Must be a king. And the
+			hat. She's a witch!</p>
+		<ul>
+			<li>No, no, no! Yes, yes. A bit. But she's got a wart.</li>
+			<li>Well, we did do the nose.</li>
+			<li>No, no, no! Yes, yes. A bit. But she's got a wart.</li>
+		</ul>
+		<h3>Blue. No, yel&hellip;</h3>
+
+		<p>Well, I got better. Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! But you are
+			dressed as one&hellip; You don't frighten us, English pig-dogs! Go and boil your bottoms, sons of a silly
+			person! I blow my nose at you, so-called Ah-thoor Keeng, you and all your silly English
+			K-n-n-n-n-n-n-n-niggits! Shut up! Will you shut up?! Well, how'd you become king, then?</p>
+		<h4>We want a shrubbery!!</h4>
+
+		<p>Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! Now, look here, my good man. Did
+			you dress her up like this? Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! No,
+			no, no! Yes, yes. A bit. But she's got a wart. What do you mean?</p>
+		<ol>
+			<li>Well, she turned me into a newt.</li>
+			<li>And this isn't my nose. This is a false one.</li>
+			<li>We found them.</li>
+			<li>Well, Mercia's a temperate zone!</li>
+		</ol>
+		<h5>Sir Lancelot</h5>
+
+		<p>Bloody Peasant! Oh! Come and see the violence inherent in the system! Help, help, I'm being repressed! We
+			found them. And this isn't my nose. This is a false one.</p>
+
+		<h6>What a strange person</h6>
+
+		<p>I am your king. Look, my liege! Shut up! Will you shut up?! He hasn't got shit all over him. I don't want to
+			talk to you no more, you empty-headed animal food trough water! I fart in your general direction! Your
+			mother was a hamster and your father smelt of elderberries! Now leave before I am forced to taunt you a
+			second time!</p>
+
+		<hr>
+
+		<p>Burn her! How do you know she is a witch? I'm not a witch.</p>
+
+		<p>Well, we did do the nose. We found them. The Knights Who Say Ni demand a sacrifice! Well, we did do the
+			nose.</p>
+
+		<pre>html, body {
+    height: 100%;
+}
+
+.cd-fixed-background {
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    background-attachment: fixed;
+}
+
+.cd-fixed-background.img-1 {
+    background-image: url("../img/img-1.jpg");
+}
+
+.cd-fixed-background.img-2 {
+    background-image: url("../img/img-2.jpg");
+}
+
+.cd-fixed-background.img-3 {
+    background-image: url("../img/img-3.jpg");
+}</pre>
+
+		<p>I have to push the pram a lot. And the hat. She's a witch! Bring her forward! Oh! Come and see the violence
+			inherent in the system! Help, help, I'm being repressed!</p>
+
+		<p>Burn her! Bloody Peasant! Oh, ow! Burn her anyway! Where'd you get the coconuts?</p>
+
+		<p>Well, I didn't vote for you. He hasn't got shit all over him. Well, she turned me into a newt. Why? The
+			Knights Who Say Ni demand a sacrifice! We want a shrubbery!!</p>
+
+		<p>And this isn't my nose. This is a false one. Well, I didn't vote for you. The swallow may fly south with the
+			sun, and the house martin or the plover may seek warmer climes in winter, yet these are not strangers to our
+			land. I have to push the pram a lot. And this isn't my nose. This is a false one.</p>
+
+		<p>And the hat. She's a witch! Why? Burn her! Listen. Strange women lying in ponds distributing swords is no
+			basis for a system of government. Supreme executive power derives from a mandate from the masses, not from
+			some farcical aquatic ceremony.</p>
+
+		<p>Shut up! Will you shut up?! Listen. Strange women lying in ponds distributing swords is no basis for a system
+			of government. Supreme executive power derives from a mandate from the masses, not from some farcical
+			aquatic ceremony. Why? Now, look here, my good man. Well, how'd you become king, then?</p>
+
+		<p>The swallow may fly south with the sun, and the house martin or the plover may seek warmer climes in winter,
+			yet these are not strangers to our land. Shh! Knights, I bid you welcome to your new home. Let us ride to
+			Camelot! Why? Shut up! Will you shut up?! I'm not a witch. But you are dressed as one&hellip;</p>
+
+		<p>Where'd you get the coconuts? Did you dress her up like this? Well, Mercia's a temperate zone! We shall say
+			'Ni' again to you, if you do not appease us. Look, my liege! Shut up! Will you shut up?!</p>
+
+		<p>You can't expect to wield supreme power just 'cause some watery tart threw a sword at you! &hellip;Are you
+			suggesting that coconuts migrate? I dunno. Must be a king. Well, she turned me into a newt.</p>
+
+		<p>Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! I dunno. Must be a king. Well, what
+			do you want? I am your king. How do you know she is a witch?</p>
+
+		<p>The Knights Who Say Ni demand a sacrifice! Look, my liege! Be quiet! Shh! Knights, I bid you welcome to your
+			new home. Let us ride to Camelot! Now, look here, my good man. Look, my liege!</p>
+
+		<p>Be quiet! Look, my liege! Burn her anyway! You don't frighten us, English pig-dogs! Go and boil your bottoms,
+			sons of a silly person! I blow my nose at you, so-called Ah-thoor Keeng, you and all your silly English
+			K-n-n-n-n-n-n-n-niggits! We found them.</p>
+
+
+	</main>
+</div>
+
+</body>
+</html>

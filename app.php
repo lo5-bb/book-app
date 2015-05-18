9a74c6b22915eaa8c7169331630fed63ac4d3167
codecalm <codecalm@gmail.com>
2015-05-19 00:54:08 +0200

    poprawki typo

--- app.php
+++ app.php
@@ -55,6 +55,65 @@ class app {
 
 		$Parsedown = new Parsedown_Extension();
 
-		return $Parsedown->text($data);
+		$text = $data;
+
+		$text = self::formatCharacters($text);
+		$text = $Parsedown->text($text);
+		$text = self::formatSingleCharacters($text);
+
+		return $text;
+	}
+
+	private static function formatSingleCharacters($str) {
+		static $charactersTable;
+		if ( ! isset($charactersTable)) {
+			$charactersTable = array(
+				'/(\s)(z|i|a|o)\s/'			=> '$1$2&nbsp;'
+			);
+		}
+
+		return preg_replace(array_keys($charactersTable), $charactersTable, $str);
+	}
+
+	public static function formatCharacters($str)
+	{
+		static $quotesTable;
+		if ( ! isset($quotesTable))
+		{
+			$quotesTable = array(
+				// nested smart quotes, opening and closing
+				// note that rules for grammar (English) allow only for two levels deep
+				// and that single quotes are _supposed_ to always be on the outside
+				// but we'll accommodate both
+				// Note that in all cases, whitespace is the primary determining factor
+				// on which direction to curl, with non-word characters like punctuation
+				// being a secondary factor only after whitespace is addressed.
+				'/\'"(\s|$)/'					=> '&#8217;&#8221;$1',
+				'/(^|\s|<p>)\'"/'				=> '$1&#8216;&#8222;',
+				'/\'"(\W)/'						=> '&#8217;&#8221;$1',
+				'/(\W)\'"/'						=> '$1&#8216;&#8222;',
+				'/"\'(\s|$)/'					=> '&#8221;&#8217;$1',
+				'/(^|\s|<p>)"\'/'				=> '$1&#8222;&#8216;',
+				'/"\'(\W)/'						=> '&#8221;&#8217;$1',
+				'/(\W)"\'/'						=> '$1&#8222;&#8216;',
+				// single quote smart quotes
+				'/\'(\s|$)/'					=> '&#8217;$1',
+				'/(^|\s|<p>)\'/'				=> '$1&#8216;',
+				'/\'(\W)/'						=> '&#8217;$1',
+				'/(\W)\'/'						=> '$1&#8216;',
+				// double quote smart quotes
+				'/"(\s|$)/'						=> '&#8221;$1',
+				'/(^|\s|<p>)"/'					=> '$1&#8222;',
+				'/"(\W)/'						=> '&#8221;$1',
+				'/(\W)"/'						=> '$1&#8222;',
+				// apostrophes
+				"/(\w)'(\w)/"					=> '$1&#8217;$2',
+				// Em dash and ellipses dots
+				'/(\w)\-\-(\w)/'					=> '$1&#8212;$2',
+				'/(\s)\-\-(\s)/'					=> '$1&#8212;$2',
+				'/(\w)\.{3}/'					=> '$1&#8230;',
+			);
+		}
+		return preg_replace(array_keys($quotesTable), $quotesTable, $str);
 	}
 }

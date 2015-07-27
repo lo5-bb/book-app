c77936b3ac2f17a0ffd4d379484a6d7b2f4c87f0
codecalm <codecalm@gmail.com>
2015-07-27 23:27:22 +0200

    dodanie menu

--- app.php
+++ app.php
@@ -28,9 +28,10 @@ class app
 		$data = '';
 
 		foreach ($files as $file) {
-			$fileData = '  '.trim(file_get_contents($file)).'  ';
+			$fileData = '  ' . trim(file_get_contents($file)) . '  ';
 			$html = self::parseMarkdown($fileData);
-			$data .= '<section class="chapter">'.$html.'</section>'. "\n\n";
+			$html = self::generateHeaders($html);
+			$data .= '<section class="chapter">' . $html . '</section>' . "\n\n";
 		}
 
 		$menu = self::generateTOC($data);
@@ -127,20 +128,107 @@ class app
 		return preg_replace(array_keys($quotesTable), $quotesTable, $str);
 	}
 
-	public static function generateTOC($code)
-	{
+	private static function generateTOC($code) {
 		$headers = array();
 
-		if(preg_match_all('/<h([1-6])>(.*?)<\/h\\1>/si', $code, $m)) {
-			for($i=0; $i<count($m[0]); $i++) {
+		if (preg_match_all('/<h([1-6]) id="([^"]+)">(.*?)<\/h\\1>/si', $code, $m)) {
+			for ($i = 0; $i < count($m[0]); $i++) {
 				$headers[] = [
 					'no' => $m[1][$i],
-					'text' => $m[2][$i],
-					'url' => md5($m[2][$i])
+					'text' => $m[3][$i],
+					'url' => $m[2][$i]
 				];
 			}
 		}
 
 		return $headers;
+	}
+
+	private static function generateHeaders($code) {
+		$code = preg_replace_callback('/<h([1-6])>(.*?)<\/h\\1>/si', function($m){
+			return '<h'.$m[1].' id="'.self::urlTitle($m[2]).'">'.$m[2].'</h'.$m[1].'>';
+		}, $code);
+
+		return $code;
+	}
+
+
+	private static function urlTitle($title, $separator = '-')
+	{
+		$title = self::transliterateToAscii($title);
+
+		$title = preg_replace('![^' . preg_quote($separator) . 'a-z0-9\s]+!', '', strtolower($title));
+		$title = preg_replace('![' . preg_quote($separator) . '\s]+!u', $separator, $title);
+
+		return trim($title, $separator);
+	}
+
+	private static function transliterateToAscii($str, $case = 0)
+	{
+		static $UTF8_LOWER_ACCENTS = NULL;
+		static $UTF8_UPPER_ACCENTS = NULL;
+
+		$str = strip_tags($str);
+
+		$str = strtr($str, [
+			'&nbsp;' => ' ',
+			'&lt;' => ' ',
+			'&gt;' => ' '
+		]);
+
+		if ($case <= 0) {
+			if ($UTF8_LOWER_ACCENTS === NULL) {
+				$UTF8_LOWER_ACCENTS = array(
+					'à' => 'a', 'ô' => 'o', 'ď' => 'd', 'ḟ' => 'f', 'ë' => 'e', 'š' => 's', 'ơ' => 'o',
+					'ß' => 'ss', 'ă' => 'a', 'ř' => 'r', 'ț' => 't', 'ň' => 'n', 'ā' => 'a', 'ķ' => 'k',
+					'ŝ' => 's', 'ỳ' => 'y', 'ņ' => 'n', 'ĺ' => 'l', 'ħ' => 'h', 'ṗ' => 'p', 'ó' => 'o',
+					'ú' => 'u', 'ě' => 'e', 'é' => 'e', 'ç' => 'c', 'ẁ' => 'w', 'ċ' => 'c', 'õ' => 'o',
+					'ṡ' => 's', 'ø' => 'o', 'ģ' => 'g', 'ŧ' => 't', 'ș' => 's', 'ė' => 'e', 'ĉ' => 'c',
+					'ś' => 's', 'î' => 'i', 'ű' => 'u', 'ć' => 'c', 'ę' => 'e', 'ŵ' => 'w', 'ṫ' => 't',
+					'ū' => 'u', 'č' => 'c', 'ö' => 'o', 'è' => 'e', 'ŷ' => 'y', 'ą' => 'a', 'ł' => 'l',
+					'ų' => 'u', 'ů' => 'u', 'ş' => 's', 'ğ' => 'g', 'ļ' => 'l', 'ƒ' => 'f', 'ž' => 'z',
+					'ẃ' => 'w', 'ḃ' => 'b', 'å' => 'a', 'ì' => 'i', 'ï' => 'i', 'ḋ' => 'd', 'ť' => 't',
+					'ŗ' => 'r', 'ä' => 'a', 'í' => 'i', 'ŕ' => 'r', 'ê' => 'e', 'ü' => 'u', 'ò' => 'o',
+					'ē' => 'e', 'ñ' => 'n', 'ń' => 'n', 'ĥ' => 'h', 'ĝ' => 'g', 'đ' => 'd', 'ĵ' => 'j',
+					'ÿ' => 'y', 'ũ' => 'u', 'ŭ' => 'u', 'ư' => 'u', 'ţ' => 't', 'ý' => 'y', 'ő' => 'o',
+					'â' => 'a', 'ľ' => 'l', 'ẅ' => 'w', 'ż' => 'z', 'ī' => 'i', 'ã' => 'a', 'ġ' => 'g',
+					'ṁ' => 'm', 'ō' => 'o', 'ĩ' => 'i', 'ù' => 'u', 'į' => 'i', 'ź' => 'z', 'á' => 'a',
+					'û' => 'u', 'þ' => 'th', 'ð' => 'dh', 'æ' => 'ae', 'µ' => 'u', 'ĕ' => 'e', 'ı' => 'i',
+				);
+			}
+			$str = str_replace(
+				array_keys($UTF8_LOWER_ACCENTS),
+				array_values($UTF8_LOWER_ACCENTS),
+				$str
+			);
+		}
+		if ($case >= 0) {
+			if ($UTF8_UPPER_ACCENTS === NULL) {
+				$UTF8_UPPER_ACCENTS = array(
+					'À' => 'A', 'Ô' => 'O', 'Ď' => 'D', 'Ḟ' => 'F', 'Ë' => 'E', 'Š' => 'S', 'Ơ' => 'O',
+					'Ă' => 'A', 'Ř' => 'R', 'Ț' => 'T', 'Ň' => 'N', 'Ā' => 'A', 'Ķ' => 'K', 'Ĕ' => 'E',
+					'Ŝ' => 'S', 'Ỳ' => 'Y', 'Ņ' => 'N', 'Ĺ' => 'L', 'Ħ' => 'H', 'Ṗ' => 'P', 'Ó' => 'O',
+					'Ú' => 'U', 'Ě' => 'E', 'É' => 'E', 'Ç' => 'C', 'Ẁ' => 'W', 'Ċ' => 'C', 'Õ' => 'O',
+					'Ṡ' => 'S', 'Ø' => 'O', 'Ģ' => 'G', 'Ŧ' => 'T', 'Ș' => 'S', 'Ė' => 'E', 'Ĉ' => 'C',
+					'Ś' => 'S', 'Î' => 'I', 'Ű' => 'U', 'Ć' => 'C', 'Ę' => 'E', 'Ŵ' => 'W', 'Ṫ' => 'T',
+					'Ū' => 'U', 'Č' => 'C', 'Ö' => 'O', 'È' => 'E', 'Ŷ' => 'Y', 'Ą' => 'A', 'Ł' => 'L',
+					'Ų' => 'U', 'Ů' => 'U', 'Ş' => 'S', 'Ğ' => 'G', 'Ļ' => 'L', 'Ƒ' => 'F', 'Ž' => 'Z',
+					'Ẃ' => 'W', 'Ḃ' => 'B', 'Å' => 'A', 'Ì' => 'I', 'Ï' => 'I', 'Ḋ' => 'D', 'Ť' => 'T',
+					'Ŗ' => 'R', 'Ä' => 'A', 'Í' => 'I', 'Ŕ' => 'R', 'Ê' => 'E', 'Ü' => 'U', 'Ò' => 'O',
+					'Ē' => 'E', 'Ñ' => 'N', 'Ń' => 'N', 'Ĥ' => 'H', 'Ĝ' => 'G', 'Đ' => 'D', 'Ĵ' => 'J',
+					'Ÿ' => 'Y', 'Ũ' => 'U', 'Ŭ' => 'U', 'Ư' => 'U', 'Ţ' => 'T', 'Ý' => 'Y', 'Ő' => 'O',
+					'Â' => 'A', 'Ľ' => 'L', 'Ẅ' => 'W', 'Ż' => 'Z', 'Ī' => 'I', 'Ã' => 'A', 'Ġ' => 'G',
+					'Ṁ' => 'M', 'Ō' => 'O', 'Ĩ' => 'I', 'Ù' => 'U', 'Į' => 'I', 'Ź' => 'Z', 'Á' => 'A',
+					'Û' => 'U', 'Þ' => 'Th', 'Ð' => 'Dh', 'Æ' => 'Ae', 'İ' => 'I',
+				);
+			}
+			$str = str_replace(
+				array_keys($UTF8_UPPER_ACCENTS),
+				array_values($UTF8_UPPER_ACCENTS),
+				$str
+			);
+		}
+		return $str;
+
 	}
 }
